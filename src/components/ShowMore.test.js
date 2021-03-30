5a60c5466490f116b81e262985d897795eb77cbf
Elena <elenamitchenko@yandex.ru>
2021-03-30 15:53:09 +0300

    еще тесты

--- src/components/ShowMore.test.js
+++ src/components/ShowMore.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {render, screen} from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import {Router, Switch, Route} from 'react-router-dom';
+import {Router} from 'react-router-dom';
 import {createMemoryHistory} from 'history';
 import configureStore from 'redux-mock-store';
 import {Provider} from 'react-redux';
